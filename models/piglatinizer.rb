8e9adcb407a0e2ba90c4c0a51ba09f16f072624a
Mitchel Parish <mitchelparish@gmail.com>
2019-02-27 17:51:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,22 @@
 class PigLatinizer
-  attr_reader :words
 
-  def initialize(words)
-    @words = words
+  def to_pig(phrase)
+    words = phrase.split(" ")
   end
 
+  def piglatinize(sentence)
+    array = to_pig(sentence)
+    array.collect {|w| new_pig(w)}.join(" ")
+  end
 
-
+  def new_pig(word)
+    letters = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
+    first = letters[1]
+    second = letters[2] + (letters[3] || "")
+    if first.length>0
+      "#{second}#{first}ay"
+    else
+      "#{second}way"
+    end
+  end
 end
