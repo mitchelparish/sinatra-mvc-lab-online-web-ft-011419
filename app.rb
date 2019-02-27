69bba0eb6738603232ef0b4f2dd09a88449f30f2
Mitchel Parish <mitchelparish@gmail.com>
2019-02-27 17:36:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 get '/' do
-  erb :user_input 
+  erb :user_input
 end
 
 post '/piglatinize' do 
-  erb :piglatinize 
-end 
+  erb :piglatinize
+end
 
 
 end
